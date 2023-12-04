794e30089a2bff3ccfb0911654c70e081655917b
Greed <106820018+maxberglund2@users.noreply.github.com>
2023-12-04 11:05:25 +0100

    style update

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ const fs = require('fs');
 
 const multer = require('multer');
 const upload = multer({ dest: 'img/' });
+
+// Prisma
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+})
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})
